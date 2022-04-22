0b8b1d8ae458ebe5df1e3a3622c0ffe080c279bf
carolinesma <sma.caroline@ymail.com>
2022-04-22 10:25:13 -0300

    código funcional

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import serial
 
 while True: #Loop para a conexão com o Arduino
     try:  #Tenta se conectar, se conseguir, o loop se encerra
-        ser = serial.Serial('/dev/ttyUSB0', 9600)    
+        ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1)    
         break
     except:
         pass
@@ -10,10 +10,11 @@ while True: #Loop para a conexão com o Arduino
 while ser.is_open: #Loop enquanto a conexão está aberta
     msg = ser.read(1) #Recebe Syn
     mgs = int.from_bytes(msg, 'little')
+    
     if (msg == 10): #Envia Ack-Syn
       print('Solicitando reoconhecimento de conexão do Arduino...')
       ser.write(chr(20).encode())
-      msg = ser.read(3)
+      msg = ser.read(1)
       mgs = int.from_bytes(msg, 'little')
 
     if (msg == 30): #Recebe Ack
